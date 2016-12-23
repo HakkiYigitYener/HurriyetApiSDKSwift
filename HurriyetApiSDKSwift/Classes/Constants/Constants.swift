c96bc740ccc01cdc644caa2b894c1631d04fbc78
Hakkı Yiğit Yener <hyyener@hurriyet.com.tr>
2016-12-23 14:41:37 +0300

    first commit

---
+++ HurriyetApiSDKSwift/Classes/Constants/Constants.swift
@@ -0,0 +1,37 @@
+//
+//  Constants.swift
+//  HurriyetApiSwiftSDK
+//
+//  Created by Hakkı Yiğit Yener on 22.12.2016.
+//  Copyright © 2016 Hurriyet. All rights reserved.
+//
+
+import Foundation
+
+    enum HASelectable:String{
+    case Id         = "Id",
+    ContentType     = "ContentType",
+    CreatedDate     = "CreatedDate",
+    Description     = "Description",
+    Files           = "Files",
+    ModifiedDate    = "ModifiedDate",
+    Path            = "Path",
+    Title           = "Title",
+    Url             = "Url",
+    RelatedNews     = "RelatedNews",
+    Tags            = "Tags",
+    Text            = "Text",
+    Writers         = "Writers",
+    FullName        = "Fullname",
+    WriterId        = "WriterId",
+    Date            = "Date",
+    Editor          = "Editor",
+    PageNews        = "PageNews"
+}
+let kArticles           = "articles"
+let kColumns            = "columns"
+let kDate               = "date"
+let kNewsPhotoGalleries = "newsphotogalleries"
+let kPages              = "pages"
+let kPaths              = "paths"
+let kWriters            = "writers"
